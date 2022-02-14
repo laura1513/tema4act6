ae4f010a1a47fd42ae73bc76352072b8fba2387d
laura <laura@LAPTOP-MOK29U59>
2022-02-14 17:43:13 +0100

    Creación del proyecto y de los métodos

---
+++ tema4act6/src/tema4act6/Tema4act6.java
@@ -0,0 +1,16 @@
+package tema4act6;
+
+public class Tema4act6 {
+
+public static double fahrenheittocelsius(double num){
+    return (num-32)/1.8;
+}
+public static double celsiustofahrenheit(double num) {
+    return num*1.8+32;
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
